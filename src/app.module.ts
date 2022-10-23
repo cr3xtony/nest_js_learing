22b8a5cd6bef96782769c87d8bc421a7309b31cf
tony <sumonroytony@gmail.com>
2022-10-23 21:00:20 +0600

    deleted user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
@@ -12,7 +11,6 @@ import { JwtGuard } from './auth/guard';
       isGlobal: true,
     }),
     AuthModule,
-    UserModule,
     PrismaModule,
   ],
   controllers: [],
